6a6061b05203fb82960dd5704988c2eea17ddb67
Ami <s175007@nishitech.ac.jp>
2020-10-10 10:06:02 +0900

    update

---
+++ resources/views/admins/books/create.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('css')
+    <style>
+        . {
+            display: block;
+        }
+
+        .form {
+
+            padding: 8% 0 0;
+            margin: auto;
+
+        }
+
+        .form__create {
+            width: 360px;
+            /* background-color: powderblue; */
+            position: relative;
+            margin: auto;
+            padding: 40px;
+            background: #f2f2f2;
+            box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+            border-radius: 10px;
+        }
+
+        .form__button {
+            text-align: center;
+        }
+
+        .form__button button {
+            padding: 10px 20px;
+        }
+
+    </style>
+@endsection
+
+@section('content')
+    <div class="form">
+        <div class="form__create">
+            <form action="{{ route('admin.categories.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Name</label>
+                    @error('name')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                    <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        placeholder="">
+                </div>
+
+
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Icon</label>
+                    @error('icon')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                    <input type="file" name="icon" class="form-control" id="exampleInputPassword1" placeholder="">
+                </div>
+                <div class="form__button">
+                    <button type="submit" class="btn btn-primary">Tạo</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
